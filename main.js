13cdf710248f7b00c190d40fb667f52a1af7fdd9
harshv1741 <ardostree2001@outlook.com>
2022-10-04 12:35:43 +0530

    implementing palindrome functionality

--- main.js
+++ main.js
@@ -4,9 +4,22 @@ const check = document.querySelector("#submit");
 const reset = document.querySelector("#reset");
 const msg = document.querySelector("#output");
 
+const isPalindrome = (date) => {
+  plainDate = date.replaceAll("-", "");
+  //   console.log("plainDate: " + plainDate);
+  reversedDate = plainDate.split("").reverse().join("");
+  //   console.log("reversedDate: " + reversedDate);
+  if (plainDate === reversedDate) {
+    msg.innerText = "Palindrome";
+  } else {
+    msg.innerText = "notPalindrome";
+  }
+};
+
 check.addEventListener("click", () => {
   date = dob.value;
-  msg.innerText = date;
+  console.log(date);
+  isPalindrome(date);
 });
 
 reset.addEventListener("click", () => {
