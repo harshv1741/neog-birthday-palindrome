34c36cd939b48ca905f77a7c96479cc72e80ce75
harshv1741 <ardostree2001@outlook.com>
2022-10-04 14:36:44 +0530

    Adding Sound Effects to every action

--- main.js
+++ main.js
@@ -1,27 +1,68 @@
 const dob = document.querySelector("#dob");
-
 const check = document.querySelector("#submit");
 const reset = document.querySelector("#reset");
 const msg = document.querySelector("#output");
 
+const showMsg = (text, sound_selector) => {
+  const infoSE = document.querySelector("#infoSE");
+  const failSE = document.querySelector("#failSE");
+  const successSE = document.querySelector("#successSE");
+  const resetSE = document.querySelector("#resetSE");
+
+  switch (sound_selector) {
+    case "info":
+      msg.style.color = "blue";
+      msg.innerText = text;
+      infoSE.play();
+      break;
+
+    case "success":
+      msg.style.color = "green";
+      msg.innerText = text;
+      successSE.play();
+      break;
+
+    case "fail":
+      msg.style.color = "red";
+      msg.innerText = text;
+      failSE.play();
+      break;
+
+    case "reset":
+      msg.innerText = "";
+      resetSE.play();
+      break;
+
+    default:
+      console.log("No Message");
+      break;
+  }
+};
+
 const isPalindrome = (date) => {
   plainDate = date.replaceAll("-", "");
-  //   console.log("plainDate: " + plainDate);
+  // console.log("plainDate: " + plainDate);
   reversedDate = plainDate.split("").reverse().join("");
-  //   console.log("reversedDate: " + reversedDate);
+  // console.log("reversedDate: " + reversedDate);
   if (plainDate === reversedDate) {
-    msg.innerText = "Palindrome";
+    text = "The date is palindrome.";
+    showMsg(text, "success");
   } else {
-    msg.innerText = "notPalindrome";
+    text = "The date is not palindrome.";
+    showMsg(text, "fail");
   }
 };
 
 check.addEventListener("click", () => {
   date = dob.value;
-  console.log(date);
-  isPalindrome(date);
+  if (date) {
+    console.log(date);
+    isPalindrome(date);
+  } else {
+    showMsg("Please! select your date", "info");
+  }
 });
 
 reset.addEventListener("click", () => {
-  msg.innerText = "";
+  showMsg("", "reset");
 });
